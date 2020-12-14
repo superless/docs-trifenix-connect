e72d2c46b81653ff24b482a03dac5de83825e075
alejandro.iglesias <alejandro.iglesias@trifenix.io>
2020-12-14 19:06:00 -0300

    borrando para recibir info automatizada

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,7 @@
  * governing permissions and limitations under the License.
  */
 
+
 const fs = require('fs');
 const { readdirSync, statSync } = require('fs')
 const { join } = require('path')
@@ -19,18 +20,51 @@ const path = require('path')
 const globalNavFilePath = require.resolve('@adobe/gatsby-theme-aio/globalNav.json');
 const globalNav = JSON.parse(fs.readFileSync(globalNavFilePath, 'utf8'));
 
-
-const directory = `./src/pages/nuget-packages`
+// Creamos repositorios para paquetes.
+const directory_nugets = `./src/pages/nuget-packages`
+const directoryModels = `./src/pages/models`
+// paquetes nuget
 var nuget_packages = [];
+
+// modelos (incluye nuget y npm)
+var models = [];
+
+// functión para obtener la ruta de ub directorio.
 const dirs = p => readdirSync(p).filter(f => statSync(join(p, f)).isDirectory());
-const directoryPath = dirs(directory);
+
+// ruta de nugets
+const directoryPath = dirs(directory_nugets);
+
+// ruta de modelos
+const directoryModelPath = dirs(directoryModels);
+
+// recorremos cada carpeta dentro del modelo (cada carpeta es un componente)
+for (const key in directoryModelPath) {  
+  // nombre de la carpeta
+  const element = directoryModelPath[key];
+
+  // ruta relativa de menu.json
+  const menuPath = `${directoryModels}/${element}/menu.json`;  
+
+  // ruta absoluta
+  const newpath = path.resolve(menuPath);  
+
+  // obteniendo json
+  let jsn = JSON.parse(fs.readFileSync(newpath, 'utf8'));
+
+  // añadiendo el nuevo json al modelo
+  models = [...models, jsn];
+}
+
+// TODO: hacer función
 for (const key in directoryPath) {  
   const element = directoryPath[key];
-  const menuPath = `${directory}/${element}/menu.json`;  
+  const menuPath = `${directory_nugets}/${element}/menu.json`;  
   const newpath = path.resolve(menuPath);  
   let jsn = JSON.parse(fs.readFileSync(newpath, 'utf8'));
   nuget_packages = [...nuget_packages, jsn];
 }
+
 
 globalNav.menus = [];
 globalNav.console = false;
@@ -57,10 +91,14 @@ module.exports = {
         title: 'Guides',
         path: '/guides/'
       },
+      {
+        title: 'Models',
+        path: '/models/'
+      }
     ],
-    subPages: [...nuget_packages]
+    subPages: [...nuget_packages, ...models]
   },
   plugins: [`@adobe/gatsby-theme-aio`],
+  flags: {PRESERVE_WEBPACK_CACHE = true},
   pathPrefix: process.env.PATH_PREFIX || '/dev-site-documentation-template/'
 };
-console.log(module.exports);
